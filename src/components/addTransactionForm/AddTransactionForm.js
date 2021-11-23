c0b45f7abbe7e4cc26f294ee3d841ed2b1764885
DenisBlinder <denisblinder2018@gmail.com>
2021-11-23 19:36:17 +1000

    Addeed form validation

--- src/components/addTransactionForm/AddTransactionForm.js
+++ src/components/addTransactionForm/AddTransactionForm.js
@@ -1,19 +1,23 @@
-import { useRef } from 'react';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import styles from './addTransaction.module.css';
 
 const AddTransactionForm = ({historyItems, updateIncome, updateExpense}) => {
-  const textInput = useRef(null);
-  const amountInput = useRef(null);
 
-  const handleSumbit = (e) => {
+  const [text, setText] = useState('');
+  const [amount, setAmount] = useState('');
+  
+  const {register, formState: { errors }, handleSubmit} = useForm();
+
+  const onSubmit = (data, e) => {
     e.preventDefault();
 
-    const amountInputValue = +amountInput.current.value;
+    const amountInputValue = +amount;
 
     historyItems.push({
       id: Math.random(100),
-      text: textInput.current.value,
-      amount: amountInputValue >= 0 ? `+${amountInputValue}` : amountInputValue
+      text,
+      amount: amountInputValue,
     });
 
     if (amountInputValue > 0) {
@@ -23,21 +27,23 @@ const AddTransactionForm = ({historyItems, updateIncome, updateExpense}) => {
 
     updateExpense(amountInputValue);
   };
-
+  
   return (
-    <div className="transaction-form" onSubmit={handleSumbit}>
+    <div className="transaction-form" onSubmit={handleSubmit(onSubmit)}>
       <h2 className="transaction-form__subtitle">Add new transaction</h2>
-      <form action="" className="transaction-form__container">
+      <form action="#" className="transaction-form__container">
         <label className="transaction-form__field">
           <span className="transaction-form__label">Text</span>
-          <input type="text" ref={textInput} className="transaction-form__input" name="text" />
+          <input type="text" className="transaction-form__input" value={text} {...register('text', {required: true})} onChange={(e) => setText(e.currentTarget.value)} />
+          {errors.text && <span>Text is required</span>}
         </label>
         <label className="transaction-form__field">
           <span className="transaction-form__label">
             Amount <br />
             (negative - expense, positive - income)
           </span>
-          <input type="text" ref={amountInput} className="transaction-form__input" name="amount" />
+          <input type="text" className="transaction-form__input" value={amount} {...register('amount', {required: true})} onChange={(e) => setAmount(e.currentTarget.value)} />
+          {errors.amount && <span>Amount is required</span>}
         </label>
         <button className="transaction-form__submit">Add transaction</button>
       </form>
--- src/components/historyItem/HistoryItem.js
+++ src/components/historyItem/HistoryItem.js
@@ -1,11 +1,17 @@
 import styles from './historyItem.module.css';
 
 const HistoryItem = ({text, amount}) => {
+  const isAmountPositive = amount > 0;
+
+  let itemClassNames = 'history-item';
+
+  itemClassNames += isAmountPositive ? ' history-item__border_green' : ' history-item__border_green';
+  
   return (
-    <li className="history-item">
+    <li className={itemClassNames}>
       <div className="history-item__wrapper">
         <div className="history-item__text">{text}</div>
-        <div className="history-item__count">{amount}</div>
+        <div className="history-item__count">{isAmountPositive ? `+${amount}` : amount}</div>
       </div>
     </li>
   );
